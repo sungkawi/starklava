661b71bb853fd7fc5ee356ddfc0738333fdde8f0
sungkawi <156063098+sungkawi@users.noreply.github.com>
2024-04-25 17:13:16 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"starknet_getBlockByNumber","params":["latest", false],"id":1}' https://rpc.starknet.lava.build/lava-referer-5a12d72f-f679-4012-832e-858b0bf9f0c4/)
 
 
 echo "$curl_output" >> Update.tsx
